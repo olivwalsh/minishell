fa27234f7631c8c6a58d772a7fbb6917bbd27be1
owalsh <owalsh@student.42.fr>
2022-09-19 14:12:59 +0200

    :sparkles: Added some error handling and base structure for termios
    *

---
+++ src/init/terminal.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   terminal.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: owalsh <owalsh@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/19 13:57:12 by owalsh            #+#    #+#             */
+/*   Updated: 2022/09/19 14:12:49 by owalsh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	init_terminal(void)
+{
+	// get attribute to set as default -> to put back default ermios at the end 
+	// change attributes as required
+	// manage signals
+}
