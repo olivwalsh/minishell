89fc17835e32c500ae93c2af78800774d1e69c92
owalsh <owalsh@student.42.fr>
2022-10-04 11:57:36 +0200

    :bug: Fixed builtin echo
    *

--- src/builtins/ms_echo.c
+++ src/builtins/ms_echo.c
@@ -6,7 +6,7 @@
 /*   By: owalsh <owalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 11:21:20 by owalsh            #+#    #+#             */
-/*   Updated: 2022/09/16 16:19:13 by owalsh           ###   ########.fr       */
+/*   Updated: 2022/10/04 11:52:55 by owalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,23 @@
 int	ms_echo(char *cmd, char **args)
 {
 	int	i;
+	int	j;
 	int	nl;
 
-	nl = 1;
-	if (ft_strcmp("echo", cmd))
-		return (EXIT_FAILURE);
-	i = 1;
-	if (args[1] && !ft_strcmp(args[1], "-n"))
+	nl = TRUE;
+	i = 0;
+	(void)cmd;
+	while (args[++i] && args[i][0] == '-')
 	{
-		i = 2;
-		nl = 0;
+		j = 1;
+		while (args[i][j] && args[i][j] == 'n')
+			j++;
+		if (args[i][j])
+			break ;
+		nl = FALSE;
 	}
 	while (args && args[i])
-	{
-		printf("%s ", args[i]);
-		i++;
-	}
+		printf("%s ", args[i++]);
 	if (nl)
 		printf("\n");
 	return (EXIT_SUCCESS);
